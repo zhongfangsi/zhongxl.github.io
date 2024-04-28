fe888143fe0b15c3c8ccdfe8503ca53d5d3f0c28
zhongfangsi <130214599+zhongfangsi@users.noreply.github.com>
2024-04-28 23:06:56 +0800

    初始化

---
+++ rollup.config.mjs
@@ -0,0 +1,63 @@
+import dts from 'rollup-plugin-dts';
+import copy from 'rollup-plugin-copy';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import { terser } from 'rollup-plugin-terser';
+
+import packageJson from './package.json' assert { type: 'json' };
+
+export default [
+    {
+        input: 'src/index.ts',
+        output: [
+            {
+                file: packageJson.main,
+                format: 'cjs',
+                name: 'ReactQrScanner',
+                sourcemap: true,
+                globals: { react: 'React' },
+                exports: 'named'
+            },
+            {
+                file: packageJson.module,
+                format: 'esm',
+                name: 'ReactQrScanner',
+                sourcemap: true,
+                globals: { react: 'React' },
+                exports: 'named'
+            }
+        ],
+        onwarn: (warning, warn) => {
+            if (warning.code === 'THIS_IS_UNDEFINED') return;
+            warn(warning); // this requires Rollup 0.46
+        },
+        plugins: [
+            peerDepsExternal(),
+            resolve(),
+            commonjs(),
+            typescript({
+                tsconfig: './tsconfig.json',
+                exclude: ['**/stories/**']
+            }),
+            copy({
+                targets: [
+                    { src: 'src/assets/*', dest: 'dist/assets' },
+                ]
+            }),
+            terser()],
+        external: ['react', 'react-dom']
+    },
+    {
+        input: 'dist/esm/index.d.ts',
+        output: [
+            {
+                file: 'dist/index.d.ts',
+                format: 'esm',
+                chunkFileNames: '[name].js'
+            }
+        ],
+        plugins: [dts()]
+    }
+];
